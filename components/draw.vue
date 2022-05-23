a17c6d2b886522c4add3c2e0daee5b82457beed2
Kyle Henfrey <esptidus@gmail.com>
2022-05-23 09:44:51 +0100

    optimize drawing and remove cursor on leave/dc

--- components/draw.vue
+++ components/draw.vue
@@ -1,6 +1,7 @@
 <script setup>
 const { $socket } = useNuxtApp();
 
+// Canvas
 const canvas = document.createElement("canvas");
 canvas.width = 5000;
 canvas.height = 5000;
@@ -8,45 +9,47 @@ const ctx = canvas.getContext("2d");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 const pos = { x: 0, y: 0 };
-const rgb = {
-  r: Math.floor(Math.random() * 256),
-  g: Math.floor(Math.random() * 256),
-  b: Math.floor(Math.random() * 256),
+const rgb = [
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256),
+];
+
+const setPosition = (x, y) => {
+  pos.x = x;
+  pos.y = y;
 };
 
-function mousedown(e) {
-  pos.x = e.x;
-  pos.y = e.y;
-}
+const mousedown = (e) => setPosition(e.x, e.y);
 
-function mousemove(e) {
-  $socket.emit("mouse", { x: e.x, y: e.y });
-
+const mousemove = (e) => {
+  $socket.emit("mtv-mousemove", [e.x, e.y]);
   if (e.buttons !== 1) return;
 
-  $socket.emit("draw", {
-    rgb,
-    from: { x: pos.x, y: pos.y },
-    to: { x: e.x, y: e.y },
-  });
+  $socket.emit("mtv-draw", [pos.x, pos.y, e.x, e.y, ...rgb]);
+  setPosition(e.x, e.y);
+};
 
-  pos.x = e.x;
-  pos.y = e.y;
-}
+const mouseleave = () => $socket.emit("mtv-mouseleave");
 
+// Socket events
 const cursors = reactive(new Map());
 
-$socket.on("mouse", (msg) => {
-  cursors.set(msg.id, { x: msg.x, y: msg.y });
+$socket.on("mtv-mousemove", (msg) => {
+  if (msg[0] < 5 || msg[1] < 5) return cursors.delete(msg.id);
+  cursors.set(msg.id, { x: msg[0], y: msg[1] });
 });
 
-$socket.on("draw", (msg) => {
+$socket.on("mtv-mouseleave", (msg) => cursors.delete(msg));
+$socket.on("mtv-disconnect", (msg) => cursors.delete(msg));
+
+$socket.on("mtv-draw", (msg) => {
   ctx.beginPath();
   ctx.lineWidth = 2;
   ctx.lineCap = "round";
-  ctx.strokeStyle = `rgb(${msg.rgb.r}, ${msg.rgb.g}, ${msg.rgb.b})`;
-  ctx.moveTo(msg.from.x, msg.from.y);
-  ctx.lineTo(msg.to.x, msg.to.y);
+  ctx.strokeStyle = `rgb(${msg[4]}, ${msg[5]}, ${msg[6]})`;
+  ctx.moveTo(msg[0], msg[1]);
+  ctx.lineTo(msg[2], msg[3]);
   ctx.stroke();
 });
 
@@ -61,10 +64,15 @@ onMounted(() => {
       <img
         class="cursor"
         src="~/assets/cursor.png"
-        :style="`left: ${pos.x}px; top: ${pos.y}px`"
+        :style="`left: ${pos.x}px; top: ${pos.y}px;`"
       />
     </div>
 
-    <div id="draw-container" @mousedown="mousedown" @mousemove="mousemove" />
+    <div
+      id="draw-container"
+      @mousedown="mousedown"
+      @mousemove="mousemove"
+      @mouseleave="mouseleave"
+    />
   </div>
 </template>
