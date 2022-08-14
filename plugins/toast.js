3c4250f38bf63d9e8b1d486fa99717d587ca829d
Kyle Henfrey <esptidus@gmail.com>
2022-08-14 18:36:46 +0100

    don't pause toasts on focus loss

--- plugins/toast.js
+++ plugins/toast.js
@@ -3,6 +3,6 @@ import Toast, { useToast } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Toast);
+  nuxtApp.vueApp.use(Toast, { pauseOnFocusLoss: false });
   return { provide: { toast: useToast() } };
 });
